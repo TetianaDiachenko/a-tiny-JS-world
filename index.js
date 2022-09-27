e55d4c003a32513a97c5fe2a57fe334592bf8da1
Tetiana Diachenko <tetianamyhun@gmail.com>
2022-09-27 09:39:38 +0300

    added index.js for the tiny js world

--- index.js
+++ index.js
@@ -2,29 +2,67 @@ import { print } from './js/lib.js';
 /* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
    Complete the below for code reviewers' convenience:
 
-   Code repository: _put repo URL here_
+   Code repository: https://github.com/TetianaDiachenko/a-tiny-JS-world
    Web app: _put project's github pages URL here_
    */
 
 // ======== OBJECTS DEFINITIONS ========
-// Define your objects here
 
+const dog = {
+  species: "dog",
+  name: "Baff",
+  gender: "male",
+  legs: 4,
+  hands: 0,
+  saying: "Woof!",
+  friends: ["Muhtar", "Rex", "Bony"]
+};
 
+const cat = {
+  species: "cat",
+  name: "Melka",
+  gender: "female",
+  legs: 4,
+  hands: 0,
+  saying: "Meow!",
+  friends: ["Murka", "Flo", "Gaby"]
+};
+
+const woman = {
+  species: "human",
+  name: "Ann",
+  gender: "female",
+  legs: 2,
+  hands: 2,
+  saying: "Hello, girls!",
+  friends: ["Kate", "Tania", "Sofia"]
+};
+
+const man = {
+  species: "human",
+  name: "Nick",
+  gender: "male",
+  legs: 2,
+  hands: 2,
+  saying: "Hello, boys!",
+  friends: ["Andriy", "Bob", "Jack"]
+};
+
+const catWoman = {
+  species: "cat",
+  name: "Mursia",
+  gender: "female",
+  legs: 2,
+  hands: 2,
+  saying: cat.saying,
+  friends: ["Murka", "Flo", "Gaby"]
+};
+
+const creatures = [dog, cat, woman, man, catWoman];
+const objKeys = ["species", "name", "gender", "legs", "hands", "saying", "friends"];
 // ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
 
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
 
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
+creatures.forEach((creature) => {
+      print(objKeys.map((keys) => creature[keys]).join("; "));
+   });
